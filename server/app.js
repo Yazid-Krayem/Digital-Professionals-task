143b156edc4ebfbfbcc47f82febb52e0fed1af7a
rakon1 <ykrayyem@gmail.com>
2019-02-26 00:05:29 +0200

    BookType query

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+const app = express()
+
+//router
+app.use('/graphql',graphqlHTTP({
+schema
+}))
+
+
+app.listen(8080,()=>{console.log('listening')})
---
+++ server/schema/schema.js
@@ -0,0 +1,33 @@
+const graphql = require('graphql')
+
+const{GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema
+    }= graphql
+
+const BookType = new GraphQLObjectType({
+    name:'Book',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        genre:{type:GraphQLString}
+    })
+})
+const RootQuery = new GraphQLObjectType({
+    name:"RootQueryType",
+    fields:{
+        book:{
+            type:BookType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                return 
+            }
+        }
+    }
+
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
